1e83725126e338780c8935944eb7854277000360
u1170127@utah.edu <u1170127@utah.edu>
2024-10-20 11:41:10 -0600

    completes esp now setup with vest

--- lib/ESPnow/ESPnow.h
+++ lib/ESPnow/ESPnow.h
@@ -1,7 +1,5 @@
-// espnow_communication.h
-
-#ifndef ESPNOW_H
-#define ESPNOW_H
+#ifndef ESPNOW_COMMON_H
+#define ESPNOW_COMMON_H
 
 #include <stdio.h>
 #include <string.h>
@@ -12,26 +10,34 @@
 #include "esp_mac.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 
+// Define pins and other constants
+#define BUTTON_PIN 48 // GPIO for the button
+#define LED_PIN 38    // GPIO for the LED
 #define ESPNOW_TAG "ESP-NOW"
 
-// Global variables
+// Declare shared variables
 extern bool paring;
 extern uint8_t own_mac[6];
-extern uint8_t peer_mac[6];
+extern uint8_t vest_mac[6];
+extern uint8_t gun_mac[6];
 
-// Data structure for ESP-NOW message
-typedef struct {
+// Data structure to hold the message to send
+typedef struct
+{
     char msg[32];
 } espnow_data_t;
 
 // Function declarations
 void espnow_recv_cb(const esp_now_recv_info_t *info, const uint8_t *data, int len);
 void espnow_send_cb(const uint8_t *mac_addr, esp_now_send_status_t status);
-void wifi_init(void);
-void espnow_init(void);
+void wifi_init();
+void espnow_init();
 void get_own_mac_address(uint8_t *mac);
 void send_message(const char *message);
-void setupESPnow(void);
+void setupESPnow();
+void send_pairing_message();
+void configure_gpio();
 
-#endif // ESPNOW_COMMUNICATION_H
+#endif // ESPNOW_COMMON_H
